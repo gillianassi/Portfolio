4149aee2a547fef286f20616920e985ecb0b92bb
Julien Menten <julien@taglayer.com>
2022-04-28 18:46:40 +0200

    Feat: added tailwind and moved to svelte (not kit)

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<div class="h-screen w-screen flex items-center justify-center" style="background-color: #252525;">
+  <h1 class="font-mono" style="color: #EEE7F2;">This is Gillian Assi's cool website!</h1>
+</div>
+
+<style global>
+  @tailwind utilities;
+  @tailwind components;
+  @tailwind base;
+</style>
+  
