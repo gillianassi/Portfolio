744069dce4fac9c9ca7e169c6dff2ff321f8b292
gillianassi <53274850+gillianassi@users.noreply.github.com>
2022-05-04 01:20:40 +0200

    Added Intrduction to about page

--- src/Routes/about.svelte
+++ src/Routes/about.svelte
@@ -1,7 +1,35 @@
-<div>
+    
+<script>
+    import Header from '../components/Header.svelte';
+    import Footer from '../components/Footer.svelte';
+</script>
+
+<div class="bg-gBackgroundColor">
+    <div class="flex flex-col gap-4 w-screen flex items-center justify-center ">
+        <div class=" w-screen "> 
+          <div class="headerSettings flex flex-col gap-4 flex items-center justify-center">
+            <Header/>
+            <h1 class="font-mono text-gPrimaryColor" >This is Gillian Assi's cool website!</h1>
+            <div class="text-gSecondaryColor text-left"> 
+                Oh, hi there! It seems like you’ve stumbled upon my About page. Now that you’re here, you might as well keep on reading.
+                Let’s start by stating the obvious: As my domain name implies, my name is Gillian Assi. 
+                I became an <b class="text-gPrimaryColor">electronics-ICT engineer</b> and <b class="text-gPrimaryColor">Game Developer</b> because creating functional electronics and applications are both a passion and a hobby of mine. 
+                Past years, next to game implementations for <b class="text-gPrimaryColor">VR</b>, <b class="text-gPrimaryColor">Graphics</b> and <b class="text-gPrimaryColor">Engine programming</b> became the topics that intrigued me the most.  However, I will always find joy in designing game mechanics.
+                
+            </div>
+            <iframe title="vimeo-player" src="https://player.vimeo.com/video/572013911?h=2014089976" width="640" height="360" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </div>
+          <Footer/>
+      </div>
 
 </div>
 
-<script>
 
-</script>
+
+<style>
+  
+    .headerSettings {
+        height: calc(100vh * 4 / 5);
+    }
+    </style>
