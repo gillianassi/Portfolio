da5d5df2e9dbd5d0add931d64e4b6c17dcd9090a
Julien Menten <julien@taglayer.com>
2022-05-04 22:53:47 +0200

    Added markdown support

--- src/Helpers/Modal.svelte
+++ src/Helpers/Modal.svelte
@@ -1,21 +1,22 @@
 <script>
-    export let message = "test notification";
+    import {fly, fade} from 'svelte/transition';
     export let showModal = false;
     export let isHighlighted = false;
-const disableHighlight = () =>{
-    isHighlighted = false;
-}
+    const disableHighlight = () =>{
+        isHighlighted = false;
+    }
 
-const enableHighlight = () =>{
-    isHighlighted = true;
-}
+    const enableHighlight = () =>{
+        isHighlighted = true;
+    }
 </script>
 
 
 {#if showModal}
-<div class="backdrop " class:highlighted={isHighlighted} on:click|self>
-    <div class="modal !bg-gPurple" on:mouseenter={enableHighlight} on:mouseleave={disableHighlight}>
-        <p>{message}</p>
+<div class="backdrop" class:highlighted={isHighlighted} on:click|self in:fade={{duration: 50}} out:fade={{duration: 100}}>
+    <div class="modal !bg-gPurple" on:mouseenter={enableHighlight} on:mouseleave={disableHighlight} in:fly={{y: 50, delay: 100}} out:fly={{y: 50,}}>
+      
+        <slot />
     </div>
 </div>
 {/if}
@@ -26,6 +27,10 @@ const enableHighlight = () =>{
     height: 100%;
     position: fixed;
     background: rgba(0,0,0,0.8);
+    top: 0;
+    left: 0;
+    z-index: 99999;
+    @apply flex items-center;
   }
   .modal{
     padding: 10px;
@@ -34,9 +39,5 @@ const enableHighlight = () =>{
     margin: 10% auto;;
     text-align: center;
     background: white;
-  }
-  .highlighted .modal{
-    background: crimson;
-    color: white;
   }
 </style>
--- src/components/projects.svelte
+++ src/components/projects.svelte
@@ -1,44 +1,65 @@
 
 <script>
-	import { fly, fade } from 'svelte/transition';
-    $: active = "Games";
+// @ts-nocheck
+
+    import Modal from '../Helpers/Modal.svelte';
+    import { fly, fade } from 'svelte/transition';
+    // Import Markdowns here:
+    import * as Test from '../markdown/projects/test.md';
+    import * as MetroidMd from '../markdown/projects/metroid.md';
+    
     const folders = [
         {title: 'Games', 
         projects: [
             {
             image: 'https://ik.imagekit.io/gillianassi/SuperMetroid_Featured_LYXt21X6u.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1651599940563',
-            title: 'Super Metroid Remake'
+            title: 'Super Metroid Remake',
+            markdown: MetroidMd,
             },
             {
             image: 'https://ik.imagekit.io/gillianassi/SuperMetroid_Featured_LYXt21X6u.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1651599940563',
-            title: 'Windwaker Remake'
+            title: 'Windwaker Remake',
+            markdown: Test,
             },
             {
             image: 'https://ik.imagekit.io/gillianassi/SuperMetroid_Featured_LYXt21X6u.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1651599940563g',
-            title: 'Tree-Umpf'
+            title: 'Tree-Umpf',
+            markdown: Test,
             }
         ]}, 
         {title: 'Research', 
         projects:[
             {
             image: 'https://ik.imagekit.io/gillianassi/SuperMetroid_Featured_LYXt21X6u.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1651599940563',
-            title: 'Regression'
+            title: 'Regression',
+            markdown: Test,
             },
             {
             image: '/images/testLogo.png',
-            title: 'Custom Engine'
+            title: 'Custom Engine',
+            markdown: Test,
             },
             {
             image: 'https://ik.imagekit.io/gillianassi/SuperMetroid_Featured_LYXt21X6u.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1651599940563g',
-            title: 'Lock free ordered List'
+            title: 'Lock free ordered List',
+            markdown: Test,
             }
         ]}];
 
-    
+    $: active = "Games";
+    $: showModal = false; 
+    $: markdown = Test.test;
+    let highlightModal = false;
+  
+
+    const toggleModal = (project) => {
+        if(showModal === false) markdown = project.markdown.html;
+        showModal = !showModal;
+    }
 
 </script>
 
-<div class="flex flex-col items-center">
+<div class="flex flex-col items-center" id="projects">
     <div>
         <ul class="flex ">
             {#each folders as folder}
@@ -52,15 +73,16 @@
         <div class="container mx-auto">
             <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-8 transition-all place-items-center">
                 {#each folders.find(f => f.title === active).projects as category, index}
-                    <article in:fly={{delay: 100 * index, y: 50, opacity: 0, duration: 300}} out:fade={{duration: 0}}>
+                    <article in:fly={{delay: 100 * index, y: 50, opacity: 0, duration: 300}} out:fade={{duration: 0}} on:click="{toggleModal(category)}">
                         {category.title}
                         <img src="{category.image}" alt="{category.title}" class="h-64 w-64"/>
                     </article>
                 {/each}
             </div>
         </div>
-        
         {/key} 
     </div>
-   
 </div>
+<Modal {showModal} isHighlighted={highlightModal} on:click={toggleModal}> 
+    {@html markdown}
+</Modal>
