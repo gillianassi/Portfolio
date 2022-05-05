d987be1fbaa8a9465fc7897b2d89d8d13598b501
gillianassi <53274850+gillianassi@users.noreply.github.com>
2022-05-05 16:56:05 +0200

    Simple about page

--- src/Helpers/Modal.svelte
+++ src/Helpers/Modal.svelte
@@ -17,9 +17,9 @@
 
 {#if showModal}
 <div class="backdrop " class:highlighted={isHighlighted} on:click|self in:fade={{duration: 50}} out:fade={{duration: 100}}>
-    <div class="modal w-4/5 max-w-5xl " on:mouseenter={enableHighlight} on:mouseleave={disableHighlight} in:fly={{y: 50, delay: 100}} out:fly={{y: 50,}}>
+    <div class="modal w-4/5 max-w-5xl" on:mouseenter={enableHighlight} on:mouseleave={disableHighlight} in:fly={{y: 50, delay: 100}} out:fly={{y: 50,}}>
       {#if bannerImage}
-      <header class="max-h-[400px] overflow-hidden">
+      <header class="max-h-[400px] overflow-hidden ">
         <img src="{bannerImage}" alt="Modal header" class="w-full"/>
       </header>
       {/if}
@@ -43,7 +43,8 @@
     @apply flex items-center;
   }
   .modal{
-    @apply max-h-[80vh] overflow-scroll overflow-hidden bg-white mx-auto rounded-sm shadow-md;
+    @apply max-h-[80vh] overflow-x-hidden overflow-scroll  overflow-hidden bg-white mx-auto rounded-sm shadow-md rounded-xl;
+    
   }
 
   .markdown-body {
--- src/Routes/about.svelte
+++ src/Routes/about.svelte
@@ -3,23 +3,27 @@
     import Header from '../components/Header.svelte';
 </script>
 
-<div class="bg-gBackgroundColor h-screen">
-    <div class="flex flex-col gap-4 w-screen flex items-center justify-center ">
-        <div class=" w-screen "> 
-          <div class="headerSettings flex flex-col gap-4 flex items-center justify-center">
-            <h1 class="font-mono text-gPrimaryColor" >This is Gillian Assi's cool website!</h1>
-            <div class="text-gSecondaryColor text-left"> 
-                Oh, hi there! It seems like you’ve stumbled upon my About page. Now that you’re here, you might as well keep on reading.
-                Let’s start by stating the obvious: As my domain name implies, my name is Gillian Assi. 
-                I became an <b class="text-gPrimaryColor">electronics-ICT engineer</b> and <b class="text-gPrimaryColor">Game Developer</b> because creating functional electronics and applications are both a passion and a hobby of mine. 
-                Past years, next to game implementations for <b class="text-gPrimaryColor">VR</b>, <b class="text-gPrimaryColor">Graphics</b> and <b class="text-gPrimaryColor">Engine programming</b> became the topics that intrigued me the most.  However, I will always find joy in designing game mechanics.
-                
-            </div>
-          </div>
+<div class="bg-gBackgroundColor h-[100vh]">
+    <div class="flex flex-col h-full gap-4 w-screen flex items-center justify-center ">
+
+      <div class="content flex flex-wrap py-2 gap-2 m-auto w-full w-4/5 bg-gSecondaryColor rounded-sm">
+        <div class="item-body px-1 sm:w-1/2">
+          <div class="font-mono text-4xl sm:text-5xl lg:text-6xl pl-10  font-changa">
+            GILLIAN ASSI
         </div>
+          <div>
+            Oh, hi there! It seems like you’ve stumbled upon my About page. Now that you’re here, you might as well keep on reading.
+            Let’s start by stating the obvious: As my domain name implies, my name is Gillian Assi. 
+            I became an <b class="text-gPrimaryColor">electronics-ICT engineer</b> and <b class="text-gPrimaryColor">Game Developer</b> because creating functional electronics and applications are both a passion and a hobby of mine. 
+            Past years, next to game implementations for <b class="text-gPrimaryColor">VR</b>, <b class="text-gPrimaryColor">Graphics</b> and <b class="text-gPrimaryColor">Engine programming</b> became the topics that intrigued me the most.  However, I will always find joy in designing game mechanics.
+          </div>
+
+        </div>
+        <img class="h-96 w-flex item-body px-2 content-center" src="https://ik.imagekit.io/gillianassi/ProfilePicture_K4y6avmEt.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1651757031924" alt="Profile"/>
+      </div>
+
       </div>
       <div>
-        <Footer/>
       </div>  
 
 </div>
