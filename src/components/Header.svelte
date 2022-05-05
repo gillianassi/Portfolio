739dc152bf579e31c01bd8712b4da5588cb557a1
gillianassi <53274850+gillianassi@users.noreply.github.com>
2022-05-05 03:02:34 +0200

    typo

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,7 +2,7 @@
     <div>
         <div class=" bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
             <p class="font-bold">Note:</p>
-            <p>This site is still under development</p>
+            <p>This website is still under development</p>
           </div>
         </div>
     
