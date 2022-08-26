8d0fd0e7759c6846a14f8c00f3d2bdf9246f3124
JulleBe <menten.julien@outlook.be>
2022-08-26 23:53:52 +0200

    hides contactform

--- src/Routes/contact.svelte
+++ src/Routes/contact.svelte
@@ -9,9 +9,8 @@
 
         <div class="flex w-full min-h-screen justify-center items-center">
             <div class="flex flex-col space-y-6 bg-purple-100 w-full max-w-4xl p-8 rounded-xl shadow-lg text-white
-                md:flex-row md:space-x-6 md:space-y-0">
-                <div>
-                </div>
+             ">
+           
                     <div class="flex flex-col justify-between">
                         <hi class="font-bold text-4xl tracking-wide text-gPrimaryColor">Contact me</hi>
                         <div class="relative w-24 ">
@@ -26,7 +25,7 @@
                                 <span class="h-8 text-gPrimaryColor">
                                     <svelte:component this={IoMdMail}  />
                                 </span>
-                                <span class="text-black">assigillian@gmail.com</span>
+                                <a class="text-black" href="mailto:assigillian@gmail.com">assigillian@gmail.com</a>
                             </div>
                         </div>
                         <div class="socials flex gap-4 ">
@@ -42,7 +41,7 @@
                         </div>
                     </div>
                 <div>
-                    <div class="bg-purple-200 rounded-xl shadow-lg p-8 text-gray-600  md:w-80">
+                    <div class="bg-purple-200 rounded-xl shadow-lg p-8 text-gray-600  md:w-80 hidden">
                         <form action="" class="flex flex-col space-y-4">
                             <div>
                                 <label for="" class="text-sm">Your Name</label>
