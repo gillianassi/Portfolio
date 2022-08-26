9b7e424fa7c10b63e9a9e573ff2506233666fdcc
JulleBe <menten.julien@outlook.be>
2022-08-26 21:33:28 +0200

    Fixed image

--- src/Routes/index.svelte
+++ src/Routes/index.svelte
@@ -5,13 +5,15 @@
 </script>
    
 <main class="flex flex-col gap-4 w-screen flex items-center justify-center bg-gBackgroundColor"> 
-  <div class="bg-cover bg-center  bg-[url('https://ik.imagekit.io/gillianassi/Banner_W9VgRcd5u.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1651747432747')]">
-    <div class="bg-gradient-to-b from-black w-screen  h-[90vh]">
-      <div class=" headerSettings  h-[90vh]">
+  <div class="bg-cover bg-center ">
+   
+    <div class=" w-screen relative h-[90vh]">
+     
+      <div class=" headerSettings absolute z-10 bg-gradient-to-b from-black w-full h-full">
         <Header/>
       </div>
+      <img class="h-full w-full absolute left-0 top-0 -z-auto" alt="hero" src="https://ik.imagekit.io/gillianassi/Banner_W9VgRcd5u.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1651747432747%22" />
     </div>
-
   </div>
 
 
