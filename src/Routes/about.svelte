69df93f52ef63bb9195fbd8a8927dc4d007a86a1
Gillian Assi <53274850+gillianassi@users.noreply.github.com>
2022-08-27 21:41:20 +0200

    Added contact Form + Expanding about

--- src/Routes/about.svelte
+++ src/Routes/about.svelte
@@ -3,12 +3,12 @@
     import Header from '../components/Header.svelte';
 </script>
 
-<div class="bg-gBackgroundColor h-[100vh]">
+<div class="bg-gBackgroundColor h-[90vh]">
     <div class="flex flex-col h-full gap-4 w-screen flex items-center justify-center ">
 
       <div class="content flex flex-rox justify-center flex-wrap py-2 gap-2 m-auto max-w-screen-lg w-auto w-4/5 max-w-[90vw] bg-gSecondaryColor rounded-sm mx-10 ">
         <div class="item-body p-4 max-w-prose" >
-          <h1 class="text-4xl text-prose pl-10 font-changa">
+          <h1 class="title text-gPrimaryColor">
             GILLIAN ASSI
           </h1>
           <p class="text-justify p-3 w-prose">
@@ -23,11 +23,78 @@
           <img class="h-96 w-flex item-body px-2 content-center" src="https://ik.imagekit.io/gillianassi/ProfilePicture_K4y6avmEt.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1651757031924" alt="Profile"/>
         </div>
       </div>
+    </div>
+</div>
+<div class="bg-neutral-900 h-[100vh]">
 
+  
+  <div class="grid-container text-purple-200 pt-10">
+    <div class="">
+      <div id="Education" class="pt-10">
+        
+        <h1 class="title pb-5">
+          Education
+        </h1>
+          
+        <div class="overflow-x-auto relative shadow-md sm:rounded-lg pl-2">
+          <table class="table-auto w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr class="">
+                <th scope="col" class="py-3 px-6">Degree</th>
+                <th scope="col" class="py-3 px-6">Specialisation</th>
+                <th scope="col" class="py-3 px-6">Year</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <td>Bachelor degree <b class="text-gPrimaryColor">Digital Arts & Entertainment</b></td>
+                <td>Game Development</td>
+                <td>2020 - now</td>
+              </tr>
+              <tr class="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
+                <td>Master of <b class="text-gPrimaryColor">Electronics and ICT Engineering Technology</b></td>
+                <td>Information and Communication Techniques</td>
+                <td>2019 – 2020</td>
+              </tr>
+              <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <td>Bachelor of <b class="text-gPrimaryColor">Engineering Technology</b></td>
+                <td>Specialization: Electronics-ICT</td>
+                <td>2016 – 2019</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-      <div>
-      </div>  
+      <div id="ProgrammingLanguages" class="pt-10">
+        <h1 class="title">
+          Learned Programming Languages
+        </h1>
+      </div>
+     
+      <div id="Technologies" class="pt-10">
+        <h1 class="title">
+          Technologies and software
+        </h1>
+      </div>
+    </div>
+    <div class="bg-black min-w-full rounded-lg pl-6 pt-10">
+      <h1 class="title ">
+        Experience
+      </h1>
+      Intern electronics Engineer (R&D)
+Specific tasks:
+	» Writing test code for IoT-devices
+		o Calibration techniques
+		o Optimization
+		o Process management
+	» Energy management van IoT-devices
+	» Implementing new features for IoT-devices
+	» Documenting code
+    <div>
 
+    </div>
+    </div>
+  </div>
 </div>
 
 
@@ -37,4 +104,29 @@
     .headerSettings {
         height: calc(100vh * 4 / 5);
     }
+    .grid-container{
+        
+    }
+
+
+    .grid-container-listing{
+        
+        display: inline-grid;
+        grid-template-columns: 80% 20%;
+        @apply grid-rows-3;
+    }
+
+    .title{
+      @apply underline underline-offset-8 decoration-purple-800 text-4xl font-changa pl-6;
+    }
+
+    @media screen and (min-width: 900px){
+      
+    .grid-container{
+        
+        display: grid;
+        grid-template-columns: 50% 50%;
+    }
+    }
+    
 </style>
--- src/Routes/contact.svelte
+++ src/Routes/contact.svelte
@@ -8,21 +8,28 @@
     <div class="flex flex-col h-full gap-4 w-screen flex items-center justify-center ">
 
         <div class="flex w-full min-h-screen justify-center items-center">
-            <div class="flex flex-col items-center space-y-6 bg-purple-100 w-full max-w-4xl p-8 rounded-xl shadow-lg text-white">
-           
-                    <div class="flex flex-col justify-between gap-4">
+            <div class="flex flex-col items-center space-y-6 bg-purple-100 w-full max-w-4xl p-8 rounded-xl shadow-lg text-white
+                        md:flex-row md:space-x-6 ms:space-y-0">
+                    <div class="flex flex-col justify-between gap-8">
                         <hi class="font-bold text-4xl tracking-wide text-gPrimaryColor">Contact me</hi>
                         <div class="relative w-24 ">
                             <img class="rounded-lg border border-gray-100 shadow-xl " src="https://ik.imagekit.io/gillianassi/ProfilePicture_K4y6avmEt.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1651757031924" alt="user image" />
                             <div class="absolute top-0 right-0 h-6 w-6 my-1 border-4 border-white rounded-full bg-green-400 z-2"></div>
                         </div>
-                        <p class="text-black text-sm">
-                            Feel free to contact me with any questions, feedback, or job offers.    
-                        </p> 
+                        <div class="text-black text-sm">
+                            <p>
+                                Feel free to contact me with any questions, feedback, or job offers. <br> 
+                            </p> 
+                            <b>
+                                Looking for an internship during Spring 2023.
+                            </b>
+                        </div>
+                        
                         <div>
-                            <div class="inline-flex items-center gap-4 self-end">
-                                <span class="h-8 text-gPrimaryColor">
-                                    <svelte:component this={IoMdMail}  />
+                            <div class="socials inline-flex items-center gap-4 self-end">
+                                <span class="">
+                                    
+                                    <a class="socials" href="mailto:assigillian@gmail.com"><IoMdMail/></a>
                                 </span>
                                 <a class="text-purple-800 socials" href="mailto:assigillian@gmail.com">assigillian@gmail.com</a>
                             </div>
@@ -39,24 +46,31 @@
                             </span>
                         </div>
                     </div>
+                        
                 <div>
-                    <div class="bg-purple-200 rounded-xl shadow-lg p-8 text-gray-600  md:w-80 hidden">
-                        <form action="" class="flex flex-col space-y-4">
+                    <div class="container bg-purple-200 rounded-xl shadow-lg p-8 text-gray-600  md:w-80">
+                        <form  class="flex flex-col space-y-4" action="https://formsubmit.co/assigillian@gmail.com" method="POST">
                             <div>
                                 <label for="" class="text-sm">Your Name</label>
-                                <input type="text" placeholder="Samus Aran" class="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none 
-                                focus:ring-2 focus:ring-purple-400">
+                                <input type="text" name="name" placeholder="Samus Aran" class="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none 
+                                focus:ring-2 focus:ring-purple-400" required> 
                             </div> 
                             <div>
                                 <label for="" class="text-sm">Email Adress</label>
-                                <input type="text" placeholder="SamusAran@gmail.com" class="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none 
-                                focus:ring-2 focus:ring-purple-400">
+                                <input type="email" name="email" placeholder="SamusAran@gmail.com" class="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none 
+                                focus:ring-2 focus:ring-purple-400" required>
+                            </div> 
+                            <div>
+                                <label for="" class="text-sm">Subject</label>
+                                <input type="text" name="_subject" placeholder="Dream Job Oppurtunity" class="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none 
+                                focus:ring-2 focus:ring-purple-400" required>
                             </div>
                             <div>
                                 <label for="" class="text-sm">Message</label>
-                                <textarea placeholder="I used to be an adventurer like you..." rows="4" class="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2Q outline-none 
-                                focus:ring-2 focus:ring-purple-400"></textarea>
+                                <textarea  name="message"placeholder="I used to be an adventurer like you..." rows="4" class="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2Q outline-none 
+                                focus:ring-2 focus:ring-purple-400" required></textarea>
                             </div>
+                            <input type="hidden" name="_autoresponse" value="This is an automatic response. You've Succesfully sent a mail to assigillian@gmail.com. I will try to reply as soon as possible!">
                             <button class="inline-block self-end bg-gPrimaryColor text-white font-bold rounded-lg px-6 py-2 uppercase text-sm">
                                 Submit Message
                             </button>
